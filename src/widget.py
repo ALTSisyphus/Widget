8b3431defd5444facc375f98e20d596b59661494
ALT Sisyphus <altsisyphus@gmail.com>
2025-07-10 21:41:30 +0300

    Добавлен модуль widget с функциями mask_account_card и get_date

---
+++ src/widget.py
@@ -0,0 +1,32 @@
+from .masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(data: str) -> str:
+    """
+    Маскирует номер карты или счета в зависимости от типа.
+    :param data: Строка формата "Visa Platinum 7000792289606361" или "Счет 73654108430135874305"
+    :return: Строка с замаскированным номером
+    """
+    # Определяем тип карты/счета
+    if "счет" in data.lower():
+        # Ищем начало номера счета
+        parts = data.split()
+        account_number = parts[-1]
+        return f"{' '.join(parts[:-1])} {get_mask_account(account_number)}"
+    else:
+        # Для карт
+        parts = data.split()
+        card_number = parts[-1]
+        return f"{' '.join(parts[:-1])} {get_mask_card_number(card_number)}"
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует дату из формата ISO в формат ДД.ММ.ГГГГ.
+    :param date_str: Строка с датой в формате "2024-03-11T02:26:18.671407"
+    :return: Строка с датой в формате "11.03.2024"
+    """
+    # Разделяем дату и время
+    date_part = date_str.split("T")[0]
+    # Разбиваем на компоненты
+    year, month, day = date_part.split("-")
+    return f"{day}.{month}.{year}"
